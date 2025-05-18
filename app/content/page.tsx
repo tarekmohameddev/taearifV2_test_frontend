b5e1ec35da0c26b7fe3755d4fd8c0c457d0fcad6
samiadham12 <102438847+samiadham12@users.noreply.github.com>
2025-05-18 23:42:40 +0300

    Update page.tsx
    .

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,6 +1,6 @@
 import { ContentManagementPage } from "@/components/content/TheContentPage/content-management-page";
 export const metadata = {
-  title: "Content",
+  title: "المحتوى",
 };
 export default function Page() {
   return <ContentManagementPage />;
